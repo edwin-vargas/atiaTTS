d6747c0f73e9e2cfb3359ebf2c754675fc633656
edwin-vargas <edwin548vargas@gmail.com>
2025-04-10 09:51:54 -0600

    no se we

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,15 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod espeaker;
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Ya jala la pinshi API, buenas noches")
+    espeaker::generate();
+    HttpResponse::Ok().body("Hello from server")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server on http://localhost:3000");
+    println!("Server >> http://localhost:3000");
     HttpServer::new(|| {
         App::new()
             .service(hello)
