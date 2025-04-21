b7b79579260b9ef3e9873657bb7776b85ee9910a
edwin-vargas <edwin548vargas@gmail.com>
2025-04-21 02:50:25 +0000

    debug

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,13 @@
 use actix_web::{
     web, 
     App, 
-    HttpServer,
-    middleware::Logger
+    HttpServer
 };
 use actix_files as fs;
-use actix_cors::Cors;
 
 mod db;
 mod tts_api;  
-use tts_api::text_to_speech;
+use tts_api::plusTTS;
 mod user_api; 
 use user_api::{
     create_user, 
@@ -20,32 +18,16 @@ use user_api::{
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
-    // ¿A dónde sin DB?
-    
-    if let Err(e) = db::ensure_db_exists() {
-        eprintln!("Database initialization failed: {}", e);
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Database initialization failed"));
-    }
+    db::ensure_db_exists().ok();
 
-    println!("Server on http://127.0.0.1:5566");
+    println!("http://127.0.0.1:5566");
 
     HttpServer::new(|| {
         App::new()
-            .wrap(Logger::default())
-            .wrap(
-                Cors::default()
-                    .allow_any_origin()
-                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-                    .allowed_headers(vec!["Content-Type"])
-                    .max_age(3600),
-            )
-            
-            // API Rutas de combis
-            
             .route("/user", web::post().to(create_user))
             .route("/signin", web::post().to(signin))
             .route("/plan", web::post().to(update_plan))
-            .route("/tts", web::post().to(text_to_speech))
+            .route("/tts", web::post().to(plusTTS))
             //GET /user
             //tts/text
             //tts/files
